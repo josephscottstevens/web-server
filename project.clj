dcdb369ea325f48b1f0dca45d321c7d74d921d79
Joseph Stevens <josephscottstevens@gmail.com>
2018-09-08 15:13:50 -0400

    now has working routes

--- project.clj
+++ project.clj
@@ -1,7 +1,12 @@
-(defproject lein-demo "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]
-                 [ring/ring-devel "1.6.3"]]
-  :ring {:handler sample/handler}
-  :plugins [[lein-ring "0.12.1"]])
+(defproject compojure-example "0.1.0-SNAPSHOT"
+            :description "FIXME: write description"
+            :url "http://example.com/FIXME"
+            :dependencies [[org.clojure/clojure "1.6.0"]
+                           [compojure "1.1.8"]
+                           [cheshire "5.3.1"]]
+            :plugins [[lein-ring "0.8.11"]]
+            :ring {:handler sample/app}
+            :profiles
+            {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring-mock "0.1.5"]]}}
+)
--- src/sample.clj
+++ src/sample.clj
@@ -1,10 +1,11 @@
-(ns sample)
-
-(defn handler
-  ([request]
-    {:status 200
-     :headers {"Content-Type" "text/plain"}
-     :body (str "Hello world!" (get request :uri))
-    }
+(ns sample
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
   )
+)
+
+(defroutes app
+  (GET "/" [] "<h1>Hello World</h1>")
+  (GET "/test" [] "<h1>test</h1>")
+  (route/not-found "<h1>Page not found</h1>")
 )
