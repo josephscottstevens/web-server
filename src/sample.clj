c6ae7fec7d5dd4bcf492ce38350a4ad3a472b22e
Joseph Stevens <josephscottstevens@gmail.com>
2018-09-08 15:43:59 -0400

    now serves static file

--- src/sample.clj
+++ src/sample.clj
@@ -1,11 +1,13 @@
 (ns sample
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.util.response :as resp]
   )
 )
 
 (defroutes app
-  (GET "/" [] "<h1>Hello World</h1>")
+  (GET "/" [] (resp/redirect "/index.html"))
   (GET "/test" [] "<h1>test</h1>")
+  (route/resources "/")
   (route/not-found "<h1>Page not found</h1>")
 )
